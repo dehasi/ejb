d5665dda975ab6f5793240138851e641357dba50
Rafa <dehasi@yandex.ru>
2016-03-17 00:10:16 +0300

    @Any are done

--- cdiWeb/src/main/Java/servlets/AllBanks.java
+++ cdiWeb/src/main/Java/servlets/AllBanks.java
@@ -3,15 +3,19 @@ package servlets;
 
 import beans.banks.Bank;
 
+import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 public class AllBanks  extends HttpServlet {
-    List<Bank> banks;
+    @Inject
+    @Any
+    Instance<Bank> banks;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
--- cdiWeb/src/main/webapp/index.jsp
+++ cdiWeb/src/main/webapp/index.jsp
@@ -4,5 +4,6 @@
 <a href="raffeisen">raffeisen</a> <br>
 <a href="sber">sber</a> <br>
 <a href="db">db</a> <br>
+<a href="all">all</a> <br>
 </body>
 </html>
